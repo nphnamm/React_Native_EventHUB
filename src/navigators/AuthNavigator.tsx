d58a579b67e5a980155d1b38bd40b4a8ff475fa0
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-08-29 00:16:41 +0700

    add new properties

--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -2,10 +2,13 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {LoginScreen} from '../screens';
+import OnbroadingScreen from '../screens/auth/OnbroadingScreen';
 const AuthNavigator = () => {
   const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="OnbroadingScreen" component={OnbroadingScreen} />
+
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
     </Stack.Navigator>
   );
---
+++ src/screens/auth/OnbroadingScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OnbroadingScreen = () => {
+  return (
+    <View>
+      <Text>OnbroadingScreen</Text>
+    </View>
+  )
+}
+
+export default OnbroadingScreen
