3dd88becb56a0e5b60990c8e56205ee0d4c78693
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-09-22 23:47:52 +0700

    change icon

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,75 +1,93 @@
-import { View, Text, StyleProp, ViewStyle, TextStyle, TouchableOpacity, Button } from 'react-native';
-import React, { ReactNode } from 'react'
-import { globalStyles } from '../styles/globalStyles';
-import { appColors } from '../constants/appColors';
+import {
+  View,
+  Text,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
+import React, {ReactNode} from 'react';
+import {globalStyles} from '../styles/globalStyles';
+import {appColors} from '../constants/appColors';
 import TextComponent from './TextComponent';
-import { fontFamilies } from '../constants/fontFamilies';
+import {fontFamilies} from '../constants/fontFamilies';
 
 interface Props {
-    icon?: ReactNode;
-    text: string; 
-    type?: 'primary' | 'text' | 'link';
-    color?: string;
-    styles?: StyleProp<ViewStyle>
-    textColor?: string; 
-    textStyles?: StyleProp<TextStyle>
-    textFont?: string;
-    onPress?: ()=> void;
-    iconFlex?: 'right' | 'left';
-    disable?: boolean;
+  icon?: ReactNode;
+  text: string;
+  type?: 'primary' | 'text' | 'link';
+  color?: string;
+  styles?: StyleProp<ViewStyle>;
+  textColor?: string;
+  textStyles?: StyleProp<TextStyle>;
+  textFont?: string;
+  onPress?: () => void;
+  iconFlex?: 'right' | 'left';
+  disable?: boolean;
 }
 
-const ButtonComponent = (
-    props: Props
-) => {
-    const {
-        icon, text, textColor, textStyles, textFont, color, styles, onPress, iconFlex, type, disable,
-    } = props
+const ButtonComponent = (props: Props) => {
+  const {
+    icon,
+    text,
+    textColor,
+    textStyles,
+    textFont,
+    color,
+    styles,
+    onPress,
+    iconFlex,
+    type,
+    disable,
+  } = props;
 
-  return type === 'primary' ? (<View>
-    <TouchableOpacity
+  return type === 'primary' ? (
+    <View style={{alignItems: 'center'}}>
+      <TouchableOpacity
         disabled={disable}
         onPress={onPress}
         style={[
-            globalStyles.button,
-            globalStyles.shadow,
-            {
-                backgroundColor: color ? color : disable ? appColors.gray4 : appColors.primary,
-                marginBottom: 17, 
-                width: '90%'
-            },
-            styles
-        ]}
-    >
+          globalStyles.button,
+          globalStyles.shadow,
+          {
+            backgroundColor: color
+              ? color
+              : disable
+              ? appColors.gray4
+              : appColors.primary,
+            marginBottom: 17,
+            width: '90%',
+          },
+          styles,
+        ]}>
         {icon && iconFlex === 'left' && icon}
         <TextComponent
-        text={text}
-        color={textColor ?? appColors.white}
-        styles={[
+          text={text}
+          color={textColor ?? appColors.white}
+          styles={[
             textStyles,
             {
-                marginLeft: icon ? 12 : 0,
-                fontSize: 16,
-                textAlign:'center',
-            }
-        ]}
-        flex={icon && iconFlex === 'right' ? 1 : 0}
-        font={textFont ?? fontFamilies.medium}
+              marginLeft: icon ? 12 : 0,
+              fontSize: 16,
+              textAlign: 'center',
+            },
+          ]}
+          flex={icon && iconFlex === 'right' ? 1 : 0}
+          font={textFont ?? fontFamilies.medium}
         />
-    </TouchableOpacity>
-  </View>
+        {icon && iconFlex === 'right' && icon}
+      </TouchableOpacity>
+    </View>
   ) : (
-    
     <TouchableOpacity onPress={onPress}>
-        <TextComponent
-            text={text}
-            flex={0}
-            color={type === 'link' ? appColors.primary : appColors.text}
-        />
-  </TouchableOpacity>
-  )
-}
+      <TextComponent
+        flex={0}
+        text={text}
+        color={type === 'link' ? appColors.primary : appColors.text}
+      />
+    </TouchableOpacity>
+  );
+};
 
 export default ButtonComponent;
-
- 
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,47 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Image } from 'react-native'
+import {Lock, Sms} from 'iconsax-react-native';
+
+import React, { useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { ButtonComponent, TextComponent } from '../../components'
+import { appColors } from '../../constants/appColors'
+import InputComponent from '../../components/InputComponent'
 
 const LoginScreen = () => {
+  const [isDisable, setIsDisable] = useState(true);
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async () => {
+
+  };
   return (
     <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text>LoginScreen</Text>
-      <Button title="Login"
-      onPress={async () => await AsyncStorage.setItem('assetToken','fafa')}
-      />
+       <Image
+          source={require('../../assets/images/text-logo.png')}
+          style={{
+            width: 162,
+            height: 114,
+            marginBottom: 30,
+          }}
+        />
+           <InputComponent
+          value={email}
+          placeholder="Email"
+          onChange={val => setEmail(val)}
+          allowClear
+          affix={<Sms size={22} color={appColors.gray} />}
+        />
+          <ButtonComponent
+          disable={isDisable}
+          onPress={handleLogin}
+          text="SIGN IN"
+          type="primary"
+        />
+          <TextComponent text="Don’t have an account? " />
+          <ButtonComponent
+            type="link"
+            text="Sign up"
+          />
     </View>
   )
 }
