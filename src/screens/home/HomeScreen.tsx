7fead168141c5734775ec15735f49b7e27f0577d
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-10-16 12:28:24 +0700

    add geolocation

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -14,11 +14,17 @@ import CategoriesList from '../../components/CategoriesList'
 import EventItem from '../../components/EventItem'
 import TagBarComponent from '../../components/TagBarComponent'
 import { AddressModel } from '../../models/AddressModel'
-
+import Geolocation from '@react-native-community/geolocation'
 const HomeScreen = ({ navigation }: any) => {
   const dispatch = useDispatch();
   const auth = useSelector(authSelector);
 
+  useEffect(() => {
+    Geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    })
+
+  }, [])
   const [currentLocation, setCurrentLocation] = useState<AddressModel>()
   const itemEvent = {
     title: 'International Band Music Concert',
@@ -37,181 +43,181 @@ const HomeScreen = ({ navigation }: any) => {
   };
   return (
     <View style={[globalStyles.container]}>
-    <StatusBar barStyle={'light-content'} />
+      <StatusBar barStyle={'light-content'} />
 
-    <View
-      style={{
-        backgroundColor: appColors.primary,
-        height: 178 + (Platform.OS === 'ios' ? 16 : 0),
-        borderBottomLeftRadius: 40,
-        borderBottomRightRadius: 40,
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 52,
-      }}>
-      <View style={{paddingHorizontal: 16}}>
-        <RowComponent>
-          <TouchableOpacity onPress={() => navigation.openDrawer()}>
-            <HambergerMenu size={24} color={appColors.white} />
-          </TouchableOpacity>
-          <View style={[{flex: 1, alignItems: 'center'}]}>
-            <RowComponent>
+      <View
+        style={{
+          backgroundColor: appColors.primary,
+          height: 178 + (Platform.OS === 'ios' ? 16 : 0),
+          borderBottomLeftRadius: 40,
+          borderBottomRightRadius: 40,
+          paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 52,
+        }}>
+        <View style={{ paddingHorizontal: 16 }}>
+          <RowComponent>
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+              <HambergerMenu size={24} color={appColors.white} />
+            </TouchableOpacity>
+            <View style={[{ flex: 1, alignItems: 'center' }]}>
+              <RowComponent>
+                <TextComponent
+                  text="Current Location"
+                  color={appColors.white2}
+                  size={12}
+                />
+                <MaterialIcons
+                  name="arrow-drop-down"
+                  size={18}
+                  color={appColors.white}
+                />
+              </RowComponent>
               <TextComponent
-                text="Current Location"
-                color={appColors.white2}
-                size={12}
-              />
-              <MaterialIcons
-                name="arrow-drop-down"
-                size={18}
+                text="New York, USA"
+                flex={0}
                 color={appColors.white}
-              />
-            </RowComponent>
-            <TextComponent
-              text="New York, USA"
-              flex={0}
-              color={appColors.white}
-              font={fontFamilies.medium}
-              size={13}
-            />
-          </View>
-
-          <CircleComponent color="#524CE0" size={36}>
-            <View>
-              <Notification size={18} color={appColors.white} />
-              <View
-                style={{
-                  backgroundColor: '#02E9FE',
-                  width: 10,
-                  height: 10,
-                  borderRadius: 4,
-                  borderWidth: 2,
-                  borderColor: '#524CE0',
-                  position: 'absolute',
-                  top: -2,
-                  right: -2,
-                }}
+                font={fontFamilies.medium}
+                size={13}
               />
             </View>
-          </CircleComponent>
-        </RowComponent>
-        <SpaceComponent height={24} />
-        <RowComponent>
-          <RowComponent
-            styles={{flex: 1}}
-            onPress={() =>
-              navigation.navigate('SearchEvents', {
-                isFilter: false,
-              })
-            }>
-            <SearchNormal1
-              variant="TwoTone"
-              size={22}
-              color={appColors.white}
-            />
-            <View
-              style={{
-                width: 1,
-                height: 18,
-                marginHorizontal: 12,
-                backgroundColor: '#A29EF0',
-              }}
-            />
-            <TextComponent text="Search..." color={`#A29EF0`} flex={1} />
-          </RowComponent>
-          <RowComponent
-            onPress={() =>
-              navigation.navigate('SearchEvents', {
-                isFilter: true,
-              })
-            }
-            styles={{
-              backgroundColor: '#5D56F3',
-              paddingHorizontal: 12,
-              paddingVertical: 8,
-              borderRadius: 100,
-            }}>
-            <CircleComponent size={19.3} color={`#A29EF0`}>
-              <Sort size={12} color={appColors.primary} />
+
+            <CircleComponent color="#524CE0" size={36}>
+              <View>
+                <Notification size={18} color={appColors.white} />
+                <View
+                  style={{
+                    backgroundColor: '#02E9FE',
+                    width: 10,
+                    height: 10,
+                    borderRadius: 4,
+                    borderWidth: 2,
+                    borderColor: '#524CE0',
+                    position: 'absolute',
+                    top: -2,
+                    right: -2,
+                  }}
+                />
+              </View>
             </CircleComponent>
-            <SpaceComponent width={8} />
-            <TextComponent text="Filters" color={appColors.white} />
           </RowComponent>
-        </RowComponent>
-        <SpaceComponent height={24} />
-      </View>
-      <View style={{marginBottom: -14}}>
-        <CategoriesList isColor />
-      </View>
-    </View>
-    <ScrollView
-      style={[
-        {
-          flex: 1,
-          paddingTop: 40,
-        },
-      ]}>
-      <SectionComponent styles={{paddingHorizontal: 0, paddingTop: 24}}>
-
-      <TagBarComponent title="Upcoming Events" onPress={() => {}} />
-      <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={Array.from({length: 5})}
-        renderItem={({item, index}) => (
-          <EventItem key={`event${index}`} item={itemEvent} type="card" />
-        )}      />
-      </SectionComponent>
-      <SectionComponent>
-      <ImageBackground 
-        source={require('../../assets/images/invite-image.png')}
-        style={{flex:1, padding:16, minHeight:127}}
-        imageStyle={{
-          resizeMode:'cover',
-          borderRadius:12,
-        }}
-
-      >
-        <TextComponent text='Invite your friends' title/>
-        <TextComponent text="Get $20 for ticket"/>
-        <RowComponent>
-          <TouchableOpacity
-            style={[
-              globalStyles.button,
-              {
-                marginTop:12,
-                backgroundColor:"#00F8FF",
-                paddingHorizontal:28,
+          <SpaceComponent height={24} />
+          <RowComponent>
+            <RowComponent
+              styles={{ flex: 1 }}
+              onPress={() =>
+                navigation.navigate('SearchEvents', {
+                  isFilter: false,
+                })
+              }>
+              <SearchNormal1
+                variant="TwoTone"
+                size={22}
+                color={appColors.white}
+              />
+              <View
+                style={{
+                  width: 1,
+                  height: 18,
+                  marginHorizontal: 12,
+                  backgroundColor: '#A29EF0',
+                }}
+              />
+              <TextComponent text="Search..." color={`#A29EF0`} flex={1} />
+            </RowComponent>
+            <RowComponent
+              onPress={() =>
+                navigation.navigate('SearchEvents', {
+                  isFilter: true,
+                })
               }
-            ]}
+              styles={{
+                backgroundColor: '#5D56F3',
+                paddingHorizontal: 12,
+                paddingVertical: 8,
+                borderRadius: 100,
+              }}>
+              <CircleComponent size={19.3} color={`#A29EF0`}>
+                <Sort size={12} color={appColors.primary} />
+              </CircleComponent>
+              <SpaceComponent width={8} />
+              <TextComponent text="Filters" color={appColors.white} />
+            </RowComponent>
+          </RowComponent>
+          <SpaceComponent height={24} />
+        </View>
+        <View style={{ marginBottom: -14 }}>
+          <CategoriesList isColor />
+        </View>
+      </View>
+      <ScrollView
+        style={[
+          {
+            flex: 1,
+            paddingTop: 40,
+          },
+        ]}>
+        <SectionComponent styles={{ paddingHorizontal: 0, paddingTop: 24 }}>
+
+          <TagBarComponent title="Upcoming Events" onPress={() => { }} />
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={Array.from({ length: 5 })}
+            renderItem={({ item, index }) => (
+              <EventItem key={`event${index}`} item={itemEvent} type="card" />
+            )} />
+        </SectionComponent>
+        <SectionComponent>
+          <ImageBackground
+            source={require('../../assets/images/invite-image.png')}
+            style={{ flex: 1, padding: 16, minHeight: 127 }}
+            imageStyle={{
+              resizeMode: 'cover',
+              borderRadius: 12,
+            }}
+
           >
-            <TextComponent
-              text='INVITE'
-              font={fontFamilies.bold}
-              color={appColors.white}
-            />
+            <TextComponent text='Invite your friends' title />
+            <TextComponent text="Get $20 for ticket" />
+            <RowComponent>
+              <TouchableOpacity
+                style={[
+                  globalStyles.button,
+                  {
+                    marginTop: 12,
+                    backgroundColor: "#00F8FF",
+                    paddingHorizontal: 28,
+                  }
+                ]}
+              >
+                <TextComponent
+                  text='INVITE'
+                  font={fontFamilies.bold}
+                  color={appColors.white}
+                />
 
-          </TouchableOpacity>
-        </RowComponent>
+              </TouchableOpacity>
+            </RowComponent>
 
-      </ImageBackground>
-    </SectionComponent>
-    <SectionComponent styles={{paddingHorizontal: 0, paddingTop: 24}}>
+          </ImageBackground>
+        </SectionComponent>
+        <SectionComponent styles={{ paddingHorizontal: 0, paddingTop: 24 }}>
 
-<TagBarComponent title="Nearby You" onPress={() => {}} />
-<FlatList
-  horizontal
-  showsHorizontalScrollIndicator={false}
-  data={Array.from({length: 5})}
-  renderItem={({item, index}) => (
-    <EventItem key={`event${index}`} item={itemEvent} type="card" />
-  )}      />
-</SectionComponent>
+          <TagBarComponent title="Nearby You" onPress={() => { }} />
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={Array.from({ length: 5 })}
+            renderItem={({ item, index }) => (
+              <EventItem key={`event${index}`} item={itemEvent} type="card" />
+            )} />
+        </SectionComponent>
 
-    </ScrollView>
-   
-   
+      </ScrollView>
 
 
-  </View>
+
+
+    </View>
 
   )
 }
